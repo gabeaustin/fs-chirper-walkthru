14c384b58a6a617a7d6e19b7cde1164eb906a85d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-22 14:21:00 -0500

    added api router info

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
+import * as path from "path";
+// import * as morgan from "morgan";
 import apiRouter from './routes';
+import config from "./config";
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+// app.use(morgan("dev"));
 app.use(apiRouter);
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
+
